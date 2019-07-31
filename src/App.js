c07e40889689869c2d658fea7dd87818de57a077
Ramakrishna Patibandla <Ramakrishna_P04@ad.infosys.com>
2019-07-30 22:40:30 -0700

    checking persons folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name : "RK", age:"35"},
+      {name : "Anu", age:"33"},
+      {name : "Sreehaas", age:"7"},
+      {name : "Vaishu", age:"1"}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //RK-***- To change the state of the component, dont directly access the state property and override it like below.
+    // DON'T do this ---- this.state.persons[0].name = "Ramakrishna";
+    this.setState({
+      persons : [
+      {name : "Ramakrishna", age:"35"},
+      {name : "Anu", age:"33"},
+      {name : "Sreehaas", age:"7"},
+      {name : "Vaishu", age:"2"}
+    ]});
+
+    //RK-***- this setState method is not defined in local class. This is in Component base class got inherited here.
+    //alert("The button was clicked");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>This is my first react app!!!</h1>
+        <button onClick = {this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I am so naughty :)</Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}>I am so Cute :)</Person>
       </div>
+      
+      //RK*** JSX compiles to below line
+      //RK*** JSX can have only one root element
+      //React.createElement('div',{className:'App'},React.createElement('h1',{className:'h1css'},'Does this really work?'))
     );
   }
 }
